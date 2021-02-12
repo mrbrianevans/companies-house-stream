7f77931774d7b7ceffa8ed83072119e498b20cb0
Brian Evans <ebrian101@gmail.com>
2021-02-12 13:08:47 +0000

    Fixed graph sql
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

--- server/graphDataGenerator.ts
+++ server/graphDataGenerator.ts
@@ -8,14 +8,14 @@ export const generateGraphData = async (req: Request, res: Response, pool: Pool)
     let sqlStatement = `
     SELECT company.minute as minute, company.count as company, filing.count as filing FROM
 (select date_trunc('minute' , f.published) as minute, count(f.published) as count from filing_events f group by date_trunc('minute' , f.published) order by date_trunc('minute' , f.published) desc limit 10) as filing
-INNER JOIN
+JOIN
 (select date_trunc('minute' , c.published) as minute, count(c.published) as count from company_events c group by date_trunc('minute' , c.published) order by date_trunc('minute' , c.published) desc limit 10) AS company
 ON filing.minute=company.minute
 ORDER BY company.minute
 ;
     `.replace(/published/g, cOrP).replace(/captured/g, cOrP) // captured or published time
         .replace(/minute/g, timeInterval).replace(/hour/g, timeInterval) // time interval
-        .replace(/limit [0-9]+/g, 'limit ' + timeInterval == 'hour' ? '72' : '1000') // set limit to 1000
+        .replace(/limit [0-9]+/g, 'limit ' + (timeInterval == 'hour' ? '72' : '1000')) // set limit to 1000
     // console.log('SQL STATEMTNS: ', sqlStatement)
     try {
         // console.time("SELECT number of events GROUP BY " + timeInterval)
