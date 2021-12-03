f541aa2af10f8e64c83b2629f59536b74776898a
Brian Evans <ebrian101@gmail.com>
2021-12-03 21:01:35 +0000

    Slightly improved event typing and added more examples of company events
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

--- server/src/types/eventTypes.ts
+++ server/src/types/eventTypes.ts
@@ -7,7 +7,9 @@ export declare module CompanyProfileEvent {
   }
 
   export interface LastAccounts {
-    made_up_to?: string
+    made_up_to?: string;
+    period_end_on: string;
+    period_start_on: string;
     type:
       | "full"
       | "small"
@@ -23,6 +25,7 @@ export declare module CompanyProfileEvent {
       | "filing-exemption-subsidiary"
       | "micro-entity"
       | "null"
+      | "unaudited-abridged";
   }
 
   export interface NextAccounts {
@@ -32,12 +35,12 @@ export declare module CompanyProfileEvent {
   }
 
   export interface Accounts {
-    accounting_reference_date: AccountingReferenceDate | {} // the |{}|{} is to satisfy samples, but should be removed in production
-    last_accounts: LastAccounts | {}
-    next_accounts: NextAccounts | {}
-    next_due: string
-    next_made_up_to: string
-    overdue?: boolean
+    accounting_reference_date: AccountingReferenceDate;
+    last_accounts: LastAccounts | { type: "null" };
+    next_accounts: NextAccounts;
+    next_due: string;
+    next_made_up_to: string;
+    overdue?: boolean;
   }
 
   export interface AnnualReturn {
@@ -102,13 +105,13 @@ export declare module CompanyProfileEvent {
   }
 
   export interface Links {
-    persons_with_significant_control?: string
-    persons_with_significant_control_statements?: string
-    registers?: string
-    self: string
-    charges?: string
-    filing_history: string
-    officers: string
+    persons_with_significant_control?: string;
+    persons_with_significant_control_statements?: string;
+    registers?: string;
+    self: string;
+    charges?: string;
+    filing_history: string;
+    officers?: string;
   }
 
   export interface PreviousCompanyName {
@@ -486,64 +489,3 @@ export declare module PscEvent {
     resource_uri: string
   }
 }
-
-// export interface CompanyProfileEvent {
-//     resource_kind: string,
-//     resource_uri: string,
-//     resource_id: string,
-//     data: {
-//         accounts: {
-//             accounting_reference_date: {
-//                 day: number,
-//                 month: number
-//             },
-//             overdue: boolean,
-//             last_accounts?: {
-//                 made_up_to: string;
-//                 type: 'full' |
-//                     'small' |
-//                     'medium' |
-//                     'group' |
-//                     'dormant' |
-//                     'interim' |
-//                     'initial' |
-//                     'total-exemption-full' |
-//                     'total-exemption-small' |
-//                     'partial-exemption' |
-//                     'audit-exemption-subsidiary' |
-//                     'filing-exemption-subsidiary' |
-//                     'micro-entity'
-//             },
-//             next_due?: string,
-//             next_made_up_to: string
-//         },
-//         can_file: boolean,
-//         company_name: string,
-//         company_number: string,
-//         company_status: string,
-//         confirmation_statement: { next_due: string, next_made_up_to: string },
-//         date_of_creation: string,
-//         etag: string,
-//         jurisdiction: string,
-//         links: {
-//             filing_history: string,
-//             officers: string,
-//             persons_with_significant_control: string,
-//             self: string
-//         },
-//         registered_office_address: {
-//             address_line_1: string,
-//             address_line_2: string,
-//             country: string,
-//             locality: string,
-//             postal_code: string
-//         },
-//         sic_codes: [string],
-//         type: string
-//     },
-//     event: {
-//         timepoint: number,
-//         published_at: string,
-//         type: string
-//     }
-// }
