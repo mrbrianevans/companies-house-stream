bfbbee7dd47ef0ad87bc2f5fe9505125e1790cb6
Brian Evans <ebrian101@gmail.com>
2022-05-20 14:50:00 +0100

    Setup solid client frontend shell
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,41 @@
-import { Component } from "solid-js"
-import "./App.scss"
+import { Component, onCleanup } from "solid-js"
+import "./styles/column_layout.scss"
 
 const App: Component = () => {
+  const socket = new WebSocket(`ws://${window.location.host}/events`)
 
+  socket.addEventListener("message", async function(event) {
+    const data = JSON.parse(event.data)
+    console.log(data) // todo: create event component
+  })
+  onCleanup(() => {
+    socket.close()
+  })
   return (
     <>
-      <h1>Companies Stream Website</h1>
-      <div class={"disconnected"} id={"connection-status"}>
-        Disconnected
+      <header>
+        <h1>Stream data from companies house in realtime</h1>
+        <div class="view-source"><code><a href="https://github.com/mrbrianevans/companies-house-stream"
+                                          target="_blank">View source code</a></code>
+        </div>
+        <div class="row">
+          <button class="disconnected" id="connection-status">Disconnected</button>
+          <div id="clock-container">
+            <div id="clock" class="bubble"></div>
+          </div>
+          <div id="counter-container">
+            <div class="bubble">Notifications: <span id="notification-counter">0</span></div>
+          </div>
+        </div>
+      </header>
+
+      <div id="events">
+        <div id="companies"><h3>Company events</h3></div>
+        <div id="filings"><h3>Filing events</h3></div>
+        <div id="officers"><h3>Officer events</h3></div>
+        <div id="psc"><h3>PSC events</h3></div>
+        <div id="charges"><h3>Charge events</h3></div>
+        <div id="insolvencies"><h3>Insolvency events</h3></div>
       </div>
     </>
   )
---
+++ client/src/eventCards/FilingEventCard.tsx
@@ -0,0 +1,7 @@
+import { Component } from "solid-js"
+
+export const FilingEventCard: Component = (props) => {
+  return <div>
+
+  </div>
+}
