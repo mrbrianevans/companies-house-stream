b3a69b28747603a818bce521f16ec444ac9c14ce
Brian Evans <ebrian101@gmail.com>
2022-08-20 15:34:14 +0100

    Allow clients to specify stream path to listen on WS
    Using the stream URL search param.
    
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

--- server/src/redis/streamFromRedis.ts
+++ server/src/redis/streamFromRedis.ts
@@ -1,25 +1,16 @@
-import { getRedisClient } from "../database/getRedisClient"
-import { PassThrough } from "stream"
+import { getRedisClient } from "../database/getRedisClient.js"
 import express from "express"
 import { WebSocketServer } from "ws"
+import { EventEmitter } from "events"
 
 const streamPaths = new Set(["companies", "filings", "officers", "persons-with-significant-control", "charges", "insolvency-cases", "disqualified-officers"])
-
 const redisClient = await getRedisClient()
-
-const str = new PassThrough({ objectMode: true })
+const eventEmitter = new EventEmitter({})
+eventEmitter.setMaxListeners(1_000_000) // increase max listeners (this is clients x num of streams)
 await redisClient.pSubscribe("event:*",
   (event, channel) => {
-    str.write(JSON.parse(event))
+    eventEmitter.emit(channel.split(":")[1], JSON.parse(event))
   })
-
-const clients = []
-str.addListener("data", event => {
-  for (const c of clients) {
-    c.send(JSON.stringify(event))
-  }
-})
-
 
 const app = express()
 
@@ -28,10 +19,6 @@ app.get("/health", async (req, res) => {
   const health = {}
   for (const streamPath of streamPaths) {
     const lastHeartbeat = await commandClient.get(streamPath + ":alive").then(t => new Date(parseInt(t || "0")))
-    console.debug({
-      lastHeartbeat,
-      streamPath
-    }, "Last heartbeat was", (Date.now() - lastHeartbeat.getTime()) / 1000, "seconds ago")
     health[streamPath] = Date.now() - lastHeartbeat.getTime() < 60_000 // more than 60 seconds indicates stream offline
   }
   await commandClient.quit()
@@ -41,26 +28,39 @@ app.get("/health", async (req, res) => {
 const server = app.listen(3000, () => console.log("Listening on port 3000"))
 server.on("request", (req) => console.log("Request to server", req.url))
 
+function getListenerCounts() {
+  const counts: Record<string, number> = {}
+  for (const streamPath of streamPaths)
+    counts[streamPath] = eventEmitter.listenerCount(streamPath)
+  return counts
+}
 
+const totalListeners = () => Object.values(getListenerCounts()).reduce((p, c) => p + c)
+let clients = 0
 // web socket server for sending events to client
 const wss = new WebSocketServer({ noServer: true })
 wss.on("connection", function connection(ws, req) {
-  clients.push(ws)
-  console.log("Websocket connected.", clients.length, "clients")
+  const stream = new URL(req.url, `wss://${req.headers.host}`).searchParams.get("stream")
+  const send = event => ws.send(JSON.stringify(event))
+  const requestedStreams = [...streamPaths].filter(streamPath => stream === streamPath || stream === null || stream === "all")
+  for (const streamPath of requestedStreams)
+    eventEmitter.addListener(streamPath, send)
+  clients++
+  console.log("Websocket connected.", totalListeners(), "event listeners", { clients })
   ws.on("close", (code, reason) => {
-    clients.splice(clients.indexOf(ws), 1)
-    console.log("Websocket disconnected.", clients.length, "clients")
+    for (const streamPath of requestedStreams)
+      eventEmitter.removeListener(streamPath, send)
+    clients--
+    console.log("Websocket disconnected.", totalListeners(), "event listeners", { clients })
   })
 })
 // handles websocket on /events path of server
 server.on("upgrade", function upgrade(request, socket, head) {
-  const { pathname } = new URL(request.url, `wss://${request.headers.host}`)
-
-  if (pathname === "/events") {
+  const url = new URL(request.url, `wss://${request.headers.host}`)
+  if (url.pathname === "/events") {
     wss.handleUpgrade(request, socket, head, function done(ws) {
       wss.emit("connection", ws, request)
     })
-
   } else {
     socket.destroy()
   }
