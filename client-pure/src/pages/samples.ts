d625daad4f361f832c4e96accac3a7081fa2a2ad
Brian Evans <ebrian101@gmail.com>
2022-12-13 15:28:19 +0000

    Added online viewer for samples
    Allows users to view a sample for a stream in a PRE code box. Simpler than downloading a file if they only want to see a single event.
    
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

--- client-pure/src/pages/samples.ts
+++ client-pure/src/pages/samples.ts
@@ -4,24 +4,43 @@ import '../styles/samples.scss'
 import '../styles/events.scss'
 
 import { streamPaths } from "../scripts/streamPaths"
-import { downloadSampleEvents } from "../scripts/downloadSampleEvents"
+import { downloadSampleEvents, getSampleEvents } from "../scripts/downloadSampleEvents"
 
 // run when samples page is loaded
 function initSamplesPage(){
-  const container = document.getElementById('streams-container')
+  { // downloads
+    const container = document.getElementById("download-streams-container")
 
-  const qtyLabel = document.createElement('label')
-  qtyLabel.innerText = 'Qty: '
-  const qtyBox = document.createElement('input')
-  qtyBox.value = '100'
-  qtyLabel.appendChild(qtyBox)
-  container.appendChild(qtyLabel)
-  for (const streamPath of streamPaths) {
-    const downloadButton = document.createElement('button')
-    downloadButton.innerText = 'Download '+streamPath + ' sample'
-    downloadButton.className = streamPath
-    downloadButton.addEventListener('click', ()=>downloadSampleEvents(streamPath, parseInt(qtyBox.value)||10))
-    container.appendChild(downloadButton)
+    const qtyLabel = document.createElement("label")
+    qtyLabel.innerText = "Qty: "
+    const qtyBox = document.createElement("input")
+    qtyBox.value = "100"
+    qtyLabel.appendChild(qtyBox)
+    container.appendChild(qtyLabel)
+    for (const streamPath of streamPaths) {
+      const downloadButton = document.createElement("button")
+      downloadButton.innerText = "Download " + streamPath + " samples"
+      downloadButton.className = streamPath
+      downloadButton.addEventListener("click", () => downloadSampleEvents(streamPath, parseInt(qtyBox.value) || 10))
+      container.appendChild(downloadButton)
+    }
+  }
+
+  { // view
+    const container = document.getElementById("view-streams-container")
+    const viewer = document.getElementById("view-samples")
+    const description = document.getElementById("sample-description")
+    for (const streamPath of streamPaths) {
+      const viewButton = document.createElement("button")
+      viewButton.innerText = "View " + streamPath + " sample"
+      viewButton.className = streamPath
+      viewButton.addEventListener("click", () => getSampleEvents(streamPath, 1).then(r=> {
+        const { received, ...event } = r[0]
+        viewer.innerText = JSON.stringify(event, null, 2)
+        description.innerText = 'Event received on '+streamPath+' stream at '+new Date(received).toISOString()+'.'
+      }))
+      container.appendChild(viewButton)
+    }
   }
 }
 
--- client-pure/src/scripts/downloadSampleEvents.ts
+++ client-pure/src/scripts/downloadSampleEvents.ts
@@ -1,8 +1,12 @@
-
-
-export async function downloadSampleEvents(streamPath: string, qty = 100){
+export async function getSampleEvents(streamPath: string, qty = 100){
+  // return [{naem:'event',received:Date.now()}]
   const query = new URLSearchParams({qty: qty.toFixed(0)})
   const events = await fetch('/events/downloadHistory/'+streamPath+'?'+query.toString()).then(r=>r.json())
+  return events
+}
+
+export async function downloadSampleEvents(streamPath: string, qty = 100){
+  const events = await getSampleEvents(streamPath, qty)
   const fileContent = getNdjson(events)
   const blob = new Blob([fileContent],{type: 'application/x-ndjson', endings: 'transparent'})
   const link = document.createElement('a');
