16aa6e3506690947a5aa87f7af71a366bd0dad39
Brian Evans <ebrian101@gmail.com>
2023-01-26 18:59:10 +0000

    Add new stream type "company-exemptions"
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

--- client-pure/src/scripts/streamPaths.ts
+++ client-pure/src/scripts/streamPaths.ts
@@ -1,1 +1,1 @@
-export const streamPaths = new Set(["companies", "filings", "officers", "persons-with-significant-control", "charges", "insolvency-cases", "disqualified-officers"])
+export const streamPaths = new Set(["companies", "filings", "officers", "persons-with-significant-control", "charges", "insolvency-cases", "disqualified-officers", "company-exemptions"])
