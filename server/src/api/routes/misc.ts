d19d91832368f51d4c726fea738c33f58f7d91c4
Brian Evans <ebrian101@gmail.com>
2024-10-26 16:57:37 +0100

    Add query param for qty of company numbers
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

--- server/src/api/routes/misc.ts
+++ server/src/api/routes/misc.ts
@@ -1,13 +1,13 @@
-import { Elysia } from "elysia"
+import { Elysia, t } from "elysia"
 import { redisClient } from "../../utils/getRedisClient"
 
 export const miscRouter = (app: Elysia) => {
   /** Returns an array of random company numbers */
-  return app.get("/randomCompanyNumbers", async () => {
-    const qty = 1 // this could be a search query param
+  return app.get("/randomCompanyNumbers", async ({ query }) => {
+    const qty = query.qty ?? 1
     const companyNumbers = await redisClient.sRandMemberCount("companyNumbers", qty)
     return companyNumbers
-  })
+  }, { query: t.Optional(t.Object({ qty: t.Number() })) })
     .get("/schemas", async () => {
       const schemasRaw = await redisClient.hGetAll("schemas")
       const schemas = Object.fromEntries(Object.entries(schemasRaw).map(([schemaName, schemaString]) => [schemaName, JSON.parse(schemaString)]))
