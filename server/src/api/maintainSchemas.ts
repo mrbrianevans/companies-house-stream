eef26ae634c3c058267d1a22a6bf63c92554a3a9
Brian Evans <ebrian101@gmail.com>
2025-10-18 17:10:57 +0100

    Start maintaining git schemas logic
    Need to install git in the docker image
    
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

--- server/src/api/maintainSchemas.ts
+++ server/src/api/maintainSchemas.ts
@@ -3,12 +3,15 @@ Listen on the Redis stream for events, and generate JSON schemas for each event 
 Keeps updating the schema based on each event, so they stay up to date.
  */
 
-import { type RedisClient, redisClient } from "../utils/getRedisClient.js"
-import { listenRedisStream } from "./listenRedisStream.js"
-import { streamPaths } from "../streams/streamPaths.js"
-import { extendSchema, createSchema } from "genson-js"
+import { type RedisClient } from "../utils/getRedisClient.js"
+import { extendSchema, createSchema, Schema } from "genson-js"
 import { streamFromRedisLogger } from "../utils/loggers.js"
 import { AnyEvent } from "../types/eventTypes"
+import { isSubset } from "genson-js/dist"
+import { existsSync } from "node:fs"
+import { simpleGit, SimpleGit, SimpleGitOptions } from "simple-git"
+import { writeFile } from "node:fs/promises"
+import { mkdirSync } from "fs"
 
 /** When an event arrives, merge it with the existing schema in redis (IF EXISTS) and save new schema. */
 export async function updateSchemaForEvent(event: AnyEvent, commandClient: RedisClient) {
@@ -19,14 +22,62 @@ export async function updateSchemaForEvent(event: AnyEvent, commandClient: Redis
   if (!savedSchema) streamFromRedisLogger.info({ resource_kind }, "Schema did not exist for event type %s", resource_kind)
   const newSchema = savedSchema ? extendSchema(savedSchema, data, { noRequired: false }) : createSchema(data)
   await commandClient.hSet("schemas", resource_kind, JSON.stringify(newSchema))
+
+  const schemaOfEvent = createSchema(data, { noRequired: false })
+  const matchesExisting = isSubset(savedSchema || {}, schemaOfEvent)
+  if (!matchesExisting) {
+    streamFromRedisLogger.info({
+      resource_kind,
+      savedSchema,
+      schemaOfEvent,
+      data
+    }, "Event did not match existing schema %s", resource_kind)
+    const newGitSchema = await addEventToSchema(event, newSchema)
+    // update schema in redis
+    await commandClient.hSet("schemas", resource_kind, JSON.stringify(newGitSchema))
+  }
 }
 
-async function maintainSchemas(signal?: AbortSignal) {
-  const eventStream = listenRedisStream({ streamKeys: [...streamPaths].map(stream => ({ stream })), signal })
 
-  for await(const event of eventStream) {
-    const parsedEvent = JSON.parse(event.data.event)
-    await updateSchemaForEvent(parsedEvent, redisClient)
+async function addEventToSchema(event: AnyEvent, newRedisSchema: Schema) {
+
+  if (!existsSync("/companies-stream-schemas")) {
+    mkdirSync("/companies-stream-schemas", { recursive: true })
   }
 
+  const options: Partial<SimpleGitOptions> = {
+    baseDir: "/companies-stream-schemas",
+    maxConcurrentProcesses: 1
+  }
+
+  const git: SimpleGit = simpleGit(options)
+
+  const alreadyCloned = await git.checkIsRepo()
+  if (!alreadyCloned) {
+    await git.clone(`https://${process.env.GITHUB_TOKEN}@github.com/mrbrianevans/companies-stream-schemas.git`, "/companies-stream-schemas")
+  }
+  // git pull to get external updates
+  await git.pull("origin", "master")
+
+  // compare schemaFromEvent against schema in repo
+  const gitSchema = await git.show("master:schemas/" + event.resource_kind + ".json")
+  const schemaOfEvent = createSchema(event, { noRequired: false })
+  const matchesExisting = gitSchema ? isSubset(JSON.parse(gitSchema), schemaOfEvent) : false
+  if (matchesExisting) {
+    streamFromRedisLogger.info({ event }, "Event already matched schema in git")
+    return JSON.parse(gitSchema)
+  }
+  // extend repo schema with event
+  const newGitSchema = gitSchema ? extendSchema(JSON.parse(gitSchema), event, { noRequired: false }) : newRedisSchema
+
+  const newSchemaContent = JSON.stringify(newGitSchema, null, 2)
+  await writeFile("/companies-stream-schemas/schemas/" + event.resource_kind + ".json", newSchemaContent)
+  await git.addConfig("user.name", "Companies House Stream")
+  await git.addConfig("user.email", "")
+  await git.add("schemas/" + event.resource_kind + ".json")
+  // commit changes. TODO: use grok for description.
+  await git.commit("Update schema " + event.resource_kind)
+  // push changes
+  await git.push("origin", "master")
+  return newGitSchema
 }
