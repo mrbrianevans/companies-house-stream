b50ae98a1dd986ba0ca0d305391feb44aaff40e8
Brian Evans <ebrian101@gmail.com>
2026-01-22 09:09:53 +0000

    Route historical API endpoint
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

---
+++ client-pure/src/pages/api.ts
@@ -0,0 +1,4 @@
+import "../styles/theme.scss"
+import "../styles/events.scss"
+import "../styles/samples.scss"
+import "../styles/layout.css"
--- reloadCaddy.sh
+++ reloadCaddy.sh
@@ -1,3 +1,4 @@
 #/bin/bash
 # to reload the caddy config with zero down time. Run from ~/app on deployment machine
+#docker compose cp ./Caddyfile webserver:/etc/caddy/Caddyfile
 docker compose exec -w /etc/caddy webserver caddy reload
