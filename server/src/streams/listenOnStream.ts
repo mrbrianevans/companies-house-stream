fec304cd24243c8953b5589bfa9bd9d951153529
BrianEvans <ebrian101@gmail.com>
2026-06-27 12:00:38 +0100

    Log response events to Redis Streams

--- server/src/streams/listenOnStream.ts
+++ server/src/streams/listenOnStream.ts
@@ -4,6 +4,9 @@ import { streamKeyHolder } from "../utils/KeyHolder.js"
 import pino from "pino"
 import { CustomJsonParse } from "./jsonParseStream.js"
 import { Readable } from "node:stream"
+import { AnyEvent } from "../types/eventTypes"
+import { redisClient } from "../utils/getRedisClient"
+import { String } from "@sinclair/typebox"
 
 export type StreamPath =
   | "insolvency-cases"
@@ -15,11 +18,39 @@ export type StreamPath =
   | "disqualified-officers"
   | string
 
+interface ResponseLogEventBase extends Record<string, string> {
+correlationId: string,
+  type: 'response_started'|'response_ended'
+  timestamp: string,
+  stream: StreamPath
+}
+interface ResponseStartedLogEvent extends ResponseLogEventBase {
+  type: 'response_started'
+  urlPath: string,
+  status: string,
+  headersObject:string,
+  requestedTimepoint: string,
+  ttfb_ms: string
+}
+interface ResponseEndedLogEvent extends ResponseLogEventBase {
+  type: 'response_ended',
+}
+type ResponseLogEvent = ResponseStartedLogEvent | ResponseEndedLogEvent
+
+const logResponse = (event: ResponseLogEvent) => redisClient.xAdd("ch:responses", "*", event, {
+  TRIM: {
+    strategy: "MAXLEN",
+    threshold: 1000,
+    strategyModifier: "~"
+  }
+})
+
 /**
  * Returns a readable stream of events. The recommended way of listening to a stream in this application.
  */
 export function stream<EventType>(streamPath: StreamPath, startFromTimepoint?: number): Readable {
-  const logger = pino({ base: { streamPath } })
+  const correlationId = crypto.randomUUID()
+  const logger = pino({ base: { streamPath, correlationId } })
   const streamKey = streamKeyHolder.useKey()
   const timepointQueryString = typeof startFromTimepoint === "number" ? `?timepoint=${startFromTimepoint}` : ""
   const path = "/" + streamPath + timepointQueryString
@@ -37,9 +68,17 @@ export function stream<EventType>(streamPath: StreamPath, startFromTimepoint?: n
   }
   parser.on("error", handleError("Error on parser stream"))
   logger.info({ path }, "Requesting to connect to stream")
+  const requestTime = Date.now()
   request(options, (res) => {
+    const ttfbMs = Date.now() - requestTime
     const { statusMessage, statusCode } = res
     logger.info({ path, res: { statusMessage, statusCode } }, "Response received from stream")
+    logResponse({
+      headersObject: JSON.stringify(res.headers),
+      status: statusCode?.toString() ?? '',
+      ttfb_ms: ttfbMs.toString(),
+      urlPath: path,
+      stream: streamPath, type: 'response_started', correlationId, timestamp: new Date().toISOString(), requestedTimepoint: startFromTimepoint?.toString()??'' })
     switch (res.statusCode) {
       case 429:
         logger.fatal("Exiting due to hitting rate limit")
@@ -57,7 +96,15 @@ export function stream<EventType>(streamPath: StreamPath, startFromTimepoint?: n
         handleError("Non 200 status code response received")
         break
     }
-    res.on("end", handleError("Stream ended. 'end' event triggered"))
+    res.on("end", (err:any) => {
+      logResponse({
+        type: 'response_ended',
+        correlationId,
+        timestamp: new Date().toISOString(),
+        stream: streamPath
+      });
+      handleError("Stream ended. 'end' event triggered")(err);
+    })
       .on("error", handleError("error on response stream"))
   })
     .on("error", handleError("Error on request"))
